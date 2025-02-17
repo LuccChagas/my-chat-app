fd8fd8c784ccde0d588166ee24bd83ce5558176a
LuccChagas <45875644+LuccChagas@users.noreply.github.com>
2025-02-17 03:26:48 -0300

    add server application entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"github.com/LuccChagas/my-chat-app/config"
+	"github.com/LuccChagas/my-chat-app/internal/websocket"
+	"github.com/joho/godotenv"
+	"log"
+)
 
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	hub := websocket.NewHub()
+	go hub.Run()
+
+	db, err := config.ConnDB()
+	if err != nil {
+		return
+	}
+
+	rabbit, err := config.ConnRabbit()
+	if err != nil {
+		return
+	}
+
+	app := config.NewApp(db, hub, rabbit)
+	app.Server.Serve()
+	log.Println("Servidor iniciado...")
+
+	defer rabbit.Close()
+	defer db.Close()
 }
